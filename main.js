0a79165fd30980be7587466e8433532b6ad55490
somto <somtocccchukwuoyima@gmail.com>
2025-08-28 06:28:50 +0100

    final changes

---
+++ main.js
@@ -0,0 +1,28 @@
+async function getWeather() {
+
+    try {
+
+        const apikey = 'bf5773950bfe4b0e80752001252808';
+        const city = document.getElementById('city-input');
+        const resultdiv = document.getElementById('weather-result');
+        if (city === '') {
+            resultdiv.innerHTML = 'Please enter a city';
+        }
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`);
+        if (!response.ok) {
+            throw new Error('City not found');
+        }
+        const data = await response.json();
+        resultDiv.innerHTML = `
+        ${data.name}, ${data.sys.country}
+        <strong>Temperature:</strong> ${data.main.temp}°C
+        <strong>Weather:</strong> ${data.weather[0].description}
+        <strong>Humidity:</strong> ${data.main.humidity}%
+        <strong>Wind Speed:</strong> ${data.wind.speed} m/s
+    `;
+        
+    } catch (error) {
+        console.error(error);
+    }
+    
+}
